923f338b26858dcb07e77908714c9a7c4a57e91a
Rabbit <Corbineau@users.noreply.github.com>
2019-05-18 13:42:08 -0400

    made some changes to reservation function

--- server.js
+++ server.js
@@ -20,84 +20,87 @@ app.use(express.json());
 //Reservation data. This is actually going to be in the database; i am including it here as an example of the data we'; be pulling.
 // =============================================================
 var reservations = [
-  {
-    id: "", //use this for the routeName
-    name: "",
-    time: "",
-    numPeople: 0,
-  },
+    {
+        id: "", //use this for the routeName
+        name: "",
+        time: "",
+        numPeople: 0,
+    },
+];
+
+var waitlist = [
+
 ];
 
 // Displays all reservations
-app.get("/api/reservations", function(req, res) {
+app.get("/api/reservations", function (req, res) {
     return res.json(reservations);
-  });
-  
-  // Displays a single reservation, or returns false -- right now, it searches by name, but the route should be the ID
-  app.get("/api/reservations/:reservation", function(req, res) {
-    var chosen = req.params.reservation;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < reservations.length; i++) {
-      if (chosen === reservations[i].name) {
-        return res.json(reservations[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
+});
 
-<<<<<<< HEAD
+// Displays a single reservation, or returns false -- right now, it searches by name, but the route should be the ID
+app.get("/api/reservations/:reservation", function (req, res) {
+    var chosen = req.params.reservation;
+
+    console.log(chosen);
+
+    for (var i = 0; i < reservations.length; i++) {
+        if (chosen === reservations[i].name) {
+            return res.json(reservations[i]);
+        }
+    }
+
+    return res.json(false);
+});
+
 //Reservation logic -- define how many tables there are, 
 
 const tables = {
     totalTables: 20,
     availTables: 20,
 
-    reserve: function() {
-        if(availTables > 0) {
-            app.post("/api/reservations", function(req, res) {
-                // req.body hosts is equal to the JSON post sent from the user
-                // This works because of our body parsing middleware
-                var newres = req.body;
-              
-                console.log(newres);
-              
-                // We then add the json the user sent to the res array
+    reserve: function () {
+
+        app.post("/api/reservations", function (req, res) {
+            // req.body hosts is equal to the JSON post sent from the user
+            // This works because of our body parsing middleware
+            var newres = req.body;
+
+            console.log(newres);
+
+            // We then add the json the user sent to the res array
+            if (this.availTables > 0) {
                 reservations.push(newres);
-              
+
                 // We then display the JSON to the users
                 res.json(newres);
-              })
-              availTables --;
-        } else {
-            console.log("no tables available.")
+                this.availTables--;
+            } else {
+                waitlist.push(rewres);
+                res.json(newres);
+                console.log("no tables available, adding to waitlist.")
+            }
+
         }
 
+        )}
     }
 
 
-}
-
-=======
->>>>>>> ed6cbd4e5005c684158341c8bf3ca977c3dfdb0d
-
 // Listener
 // ===========================================================
-app.listen(PORT, function() {
-    console.log(`App listening on PORT ${PORT}`);
-  });
+app.listen(PORT, function () {
+        console.log(`App listening on PORT ${PORT}`);
+    });
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "home.html"));
+    });
 
-app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
-});
+    app.get("/reserve", function (req, res) {
+        res.sendFile(path.join(__dirname, "reserve.html"));
+    });
 
-app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
-});
+    app.get("/tables", function (req, res) {
+        res.sendFile(path.join(__dirname, "tables.html"));
+    });
 
